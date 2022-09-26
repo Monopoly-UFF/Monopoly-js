e9995fb2e21291816670dd584b062e688ceb17c6
Gustavo Santos <gssantos@id.uff.br>
2022-09-26 16:38:59 -0300

    Configurações no server

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const bodyParser = require('body-parser')
 
-app.get('/', function (req, res) {
-    res.send("Servidor iniciado!")
+//EJS Como view engine
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+//BodyParser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Rotas
+app.get('/', (req, res) => {
+    res.render('pagina_inicial')
 })
 
 
 app.listen(port, () => {
-    console.info("Rodando em http://localhost:${port} ")
+    console.info(`Rodando em http://localhost:${port}`)
 })
