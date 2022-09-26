74583c6b915472e8c69e3a7c488d95030891cae5
Gustavo Santos <gssantos@id.uff.br>
2022-09-26 16:07:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 
 const app = express()
-const port = 4000
+const port = process.env.PORT || 3000
 
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) => {
     res.send("Servidor iniciado!")
 })
 
