db2ec5617f3533cefd724deeeec3d0a131386e27
Gustavo Santos <gssantos@id.uff.br>
2022-09-26 14:36:48 -0300

    Criado Servidor inicial

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send("Servidor iniciado!")
+})
+
+
+app.listen(port, () => {
+    console.log(`===>    Rodando em\n===>    http://localhost:${port}\n===>    CTRL+C para fechar!`)
+})
