8eaa2d3f31830f5d3355df36116633a62245e20d
Gustavo Santos <gssantos@id.uff.br>
2022-09-26 16:13:16 -0300

    Update Server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-app.get('/', function (req, res) => {
+app.get('/', function (req, res) {
     res.send("Servidor iniciado!")
 })
 
 
 app.listen(port, () => {
-    console.log(`===>    Rodando em\n===>    http://localhost:${port}\n===>    CTRL+C para fechar!`)
+    console.info("Rodando em http://localhost:${port} ")
 })
