5d72b5847e7deedbed97124d116ff70c4952ff77
lucas <lucascanellasdev@gmail.com>
2022-11-29 08:55:05 -0300

    Tradução

--- client/src/main/infraestruture/i18n/i18n.en.js
+++ client/src/main/infraestruture/i18n/i18n.en.js
@@ -9,49 +9,49 @@
 	exports.BUTTON_ADD_PLAYER = 'Adicionar Jogador';
 	
 	// Choices
-	exports.CHOICE_ROLL_DICE = 'Roll the dice';
-	exports.CHOICE_FINISH_TURN = 'Fim de Turno';
-	exports.CHOICE_BUY_PROPERTY = 'Buy {property} for {price}';
+	exports.CHOICE_ROLL_DICE = 'Jogar Dados';
+	exports.CHOICE_FINISH_TURN = 'Terminar Turno';
+	exports.CHOICE_BUY_PROPERTY = 'Comprar {property} por {price}';
 	exports.CHOICE_PAY_RENT = 'Pague {rent} para {toPlayer}';
 	exports.CHOICE_GO_BANKRUPT = 'Falido';
 	exports.CHOICE_PAY_TAX = 'Pague {amount} de imposto';
 	exports.CHOOSE_FLAT_TAX = 'Choose a flat {amount} imposto';
 	exports.CHOOSE_PERCENTAGE_TAX = 'Choose a {percentage}% imposto';
 	exports.CHOICE_CALCULATE_DICE_RENT = 'Jogue os dados e pague um aluguel de {multiplier} vezes o resultado';
-	exports.CHOICE_TRADE = "Trade with {player}";
-	exports.TRADE_MAKE_OFFER = "Make this offer";
-	exports.TRADE_CANCEL = "Cancel trade";
-	exports.CHOICE_ACCEPT_OFFER = "Accept offer";
-	exports.CHOICE_REJECT_OFFER = "Reject offer";
-	exports.CHOICE_GO_TO_JAIL = "Go to jail";
-	exports.CHOICE_PAY_DEPOSIT = "Pay a {money} deposit to get out of jail";
-	exports.CHOICE_TRY_DOUBLE_ROLL = "Try to roll a double to get out of jail";
+	exports.CHOICE_TRADE = "Negociar com {player}";
+	exports.TRADE_MAKE_OFFER = "Fazer Oferta";
+	exports.TRADE_CANCEL = "Cancelar troca";
+	exports.CHOICE_ACCEPT_OFFER = "Aceitar Oferta";
+	exports.CHOICE_REJECT_OFFER = "Rejeitar Oferta";
+	exports.CHOICE_GO_TO_JAIL = "Vá para a prisão";
+	exports.CHOICE_PAY_DEPOSIT = "Faça um depósito de {money} para sair da prisão";
+	exports.CHOICE_TRY_DOUBLE_ROLL = "Tente dois números iguais para sair da prisão";
 	
 	// Log messages
-	exports.LOG_DICE_ROLL = '{player} rolled a {die1} and a {die2}';
-	exports.LOG_DOUBLE_DICE_ROLL = '{player} rolled a double of {dice}';
-	exports.LOG_PROPERTY_BOUGHT = '{player} bought {property}';
-	exports.LOG_RENT_PAID = '{fromPlayer} paid {amount} to {toPlayer}';
-	exports.LOG_SALARY = "{player} passed GO and received $200";
-	exports.LOG_TAX_PAID = "{player} paid a {amount} tax";
+	exports.LOG_DICE_ROLL = '{player} jogou um {die1} e um {die2}';
+	exports.LOG_DOUBLE_DICE_ROLL = '{player} jogou e os dois deram {dice}';
+	exports.LOG_PROPERTY_BOUGHT = '{player} comprou {property}';
+	exports.LOG_RENT_PAID = '{fromPlayer} pagou {amount} para {toPlayer}';
+	exports.LOG_SALARY = "{player} passou pelo GO e recebeu $200";
+	exports.LOG_TAX_PAID = "{player} pagou {amount} de imposto";
 	exports.LOG_OFFER_MADE = "{player1} offered {player2} : {offer1} for {offer2}";
-	exports.LOG_OFFER_ACCEPTED = "The offer has been accepted";
-	exports.LOG_CONJUNCTION = 'and';
-	exports.LOG_OFFER_REJECTED = "The offer has been rejected";
-	exports.LOG_GONE_TO_JAIL = "{player} went to jail";
-	exports.LOG_GONE_BANKRUPT = "{player} has gone bankrupt";
-	exports.LOG_GAME_WON = "{player} has won the game";
+	exports.LOG_OFFER_ACCEPTED = "A oferta foi aceita";
+	exports.LOG_CONJUNCTION = 'e';
+	exports.LOG_OFFER_REJECTED = "A oferta foi rejeitada";
+	exports.LOG_GONE_TO_JAIL = "{player} foi para a prisão";
+	exports.LOG_GONE_BANKRUPT = "{player} faliu";
+	exports.LOG_GAME_WON = "{player} ganhou o jogo";
 	
 	// Squares
 	exports.CHANCE = 'Chance';
-	exports.COMMUNITY_CHEST = 'Community Chest';
-	exports.INCOME_TAX = 'Income Tax';
-	exports.LUXURY_TAX = 'Luxury Tax';
-	exports.LUXURY_TAX_DESCRIPTION = "Pay $75";
-	exports.INCOME_TAX_DESCRIPTION = "Pay 10% or $200";
-	exports.START_DESCRIPTION = "Collect $200 salary as you pass";
-	exports.VISITING_JAIL = "Just visiting";
-	exports.FREE_PARKING = "Free parking";
+	exports.COMMUNITY_CHEST = 'Baú da Comunidade';
+	exports.INCOME_TAX = 'Imposto de Renda';
+	exports.LUXURY_TAX = 'Imposto de Luxo';
+	exports.LUXURY_TAX_DESCRIPTION = "Pague $75";
+	exports.INCOME_TAX_DESCRIPTION = "Pague 10% ou $200";
+	exports.START_DESCRIPTION = "Passe pelo GO e receba $200";
+	exports.VISITING_JAIL = "Visitando a Prisão";
+	exports.FREE_PARKING = "Estacionamento Gratuito";
 	exports.GO_TO_JAIL = "Vá para prisao";
 	
 	exports.COMPANY_WATER = 'Companhia de agua';
@@ -86,7 +86,7 @@
 	exports.PROPERTY_BW = 'Boardwalk';
 	
 	// Player name
-	exports.DEFAULT_PLAYER_NAME = 'Player {index}';
+	exports.DEFAULT_PLAYER_NAME = 'Jogador {index}';
 	
 	// Player types
 	exports.PLAYER_TYPE_HUMAN = 'Jogador';
